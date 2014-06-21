84721990bc2d9021b326b968ce5c9b39709c8316
holyshared <holy.shared.design@gmail.com>
2014-06-21 13:55:59 +0900

    refactaring

--- src/Expectation.php
+++ src/Expectation.php
@@ -1,7 +1,66 @@
 <?php
 
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace expectation;
+
+use expectation\matcher\MatcherContainerInterface;
+
 
 class Expectation
 {
+
+    /**
+     * @var mixed
+     */
+    private $actual;
+
+    /**
+     * @var boolean
+     */
+    private $negated = false;
+
+    /**
+     * @var \expectation\matcher\MatcherContainerInterface
+     */
+    private $container;
+
+    public function __constrcut(MatcherContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    public function that($actual)
+    {
+        $this->actual = $actual;
+        return $this;
+    }
+
+    public function not()
+    {
+        $this->negated = true;
+        return $this;
+    }
+
+    public function __call($name, array $arguments)
+    {
+        if (method_exists($this, $name)) {
+            return call_user_func_array([$this, $name], $arguments);
+        }
+
+        $matcher = $this->container->find($name, $arguments);
+
+        if ($this->negated) {
+            $matcher->negativeMatch($this->actual);
+        } else {
+            $matcher->positiveMatch($this->actual);
+        }
+    }
+
 }
---
+++ src/matcher/MatcherContainer.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation\matcher;
+
+
+class MatcherContainer implements MatcherContainerInterface
+{
+
+    /**
+     * @return \expectation\matcher\MatcherInterface
+     */
+    public function find($name, array $arguments)
+    {
+    }
+
+}
