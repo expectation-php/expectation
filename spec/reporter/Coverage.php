ba0d0e94f6a46efcee4afad7a4c52b4711233561
holyshared <holy.shared.design@gmail.com>
2014-06-22 16:49:02 +0900

    add code coverage setting

---
+++ spec/reporter/Coverage.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation\spec\reporter;
+
+use Preview\Reporter\Spec;
+use PHP_CodeCoverage;
+use PHP_CodeCoverage_Report_Clover;
+
+/**
+ * @package expectation\spec\reporter
+ */
+class Coverage extends Spec
+{
+
+    private $coverage;
+
+    public function before_all($results)
+    {
+        $this->coverage = new PHP_CodeCoverage;
+        $this->coverage->filter()->addDirectoryToWhitelist('src');
+        $this->coverage->start('default');
+
+        parent::before_all($results);
+    }
+
+    public function after_all($results)
+    {
+        $this->coverage->stop();
+
+        $directoryPath = getcwd();
+        $writer = new PHP_CodeCoverage_Report_Clover;
+        $writer->process($this->coverage, $directoryPath . '/clover.xml');
+
+        parent::after_all($results);
+    }
+
+}
+
