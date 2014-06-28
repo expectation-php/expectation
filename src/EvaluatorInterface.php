60558dd6cb2d0b4e725fdfcdfb6fadfa85cf298c
holyshared <holy.shared.design@gmail.com>
2014-06-28 17:13:07 +0900

    add evaluator

---
+++ src/EvaluatorInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation;
+
+interface EvaluatorInterface
+{
+
+    /**
+     * @param mixed $actual
+     * @return $this
+     */
+    public function that($actual);
+
+    /**
+     * @return $this
+     */
+    public function not();
+
+}
