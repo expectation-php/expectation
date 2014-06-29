350e210a31e948178ba5333d52de474df33b5666
holyshared <holy.shared.design@gmail.com>
2014-06-29 14:53:39 +0900

    add dsl

---
+++ src/DSL.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation;
+
+function expect($actual)
+{
+    return Expectation::expect($actual);
+}
