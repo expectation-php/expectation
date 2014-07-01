5f2d2dfcf19de33fe26b735a57eaa06340641b67
horio_noritaka <noritaka.horio@drecom.co.jp>
2014-07-01 09:56:53 +0900

    add spec

--- spec/ConfigurableSpec.php
+++ spec/ConfigurableSpec.php
@@ -14,19 +14,32 @@ namespace Preview\DSL\BDD;
 use Assert\Assertion;
 use expectation\spec\fixture\FixtureExpectation;
 use expectation\ConfigrationBuilder;
+use stdClass;
 
 describe('Configurable', function() {
 
     describe('configure', function() {
         context('when with configurator', function() {
             before(function() {
-                FixtureExpectation::configure(function(ConfigrationBuilder $config) {
+                $this->subject = $subject = new stdClass();
+                $this->subject->callCount = 0;
+                $this->subject->callArgument = null;
+
+                FixtureExpectation::configure(function(ConfigrationBuilder $config) use($subject) {
+                    $subject->callCount++;
+                    $subject->callArgument = $config;
+
                     $config->registerMatcherNamespace('expectation\spec\fixture', __DIR__ . '/fixture');
                 });
             });
             it('should create configration', function() {
                 Assertion::isInstanceOf(FixtureExpectation::configration(), 'expectation\Configration');
             });
+/*
+            it('should configurator call once', function() {
+                Assertion::same($this->subject->callCount, 1);
+            });
+*/
         });
         context('when configurator empty', function() {
             before(function() {
