50ed0f919ca6314fde8388120272b26287b56713
holyshared <holy.shared.design@gmail.com>
2014-07-05 18:25:15 +0900

    add dsl

---
+++ spec/fixture/FixtureTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation\spec\fixture;
+
+use expectation\ExpectDSL;
+use expectation\ExpectDSLInterface;
+
+class FixtureTestCase implements ExpectDSLInterface
+{
+    use ExpectDSL;
+}
