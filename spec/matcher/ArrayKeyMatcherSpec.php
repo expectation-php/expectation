dd409e31decc7f88794e374a2d86d45a7c8bc8f6
holyshared <holy.shared.design@gmail.com>
2014-07-06 17:44:23 +0900

    add ArrayKeyMatcher

---
+++ spec/matcher/ArrayKeyMatcherSpec.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Preview\DSL\BDD;
+
+use Assert\Assertion;
+use expectation\matcher\ArrayKeyMatcher;
+use expectation\matcher\Formatter;
+
+describe('ArrayKeyMatcher', function() {
+
+    before_each(function() {
+        $this->matcher = new ArrayKeyMatcher(new Formatter());
+    });
+
+    describe('match', function() {
+        context('when same value', function() {
+            it('should return true', function() {
+                $this->matcher->expectValue = 'foo';
+                Assertion::true($this->matcher->match([
+                    'foo' => 'bar'
+                ]));
+            });
+        });
+        context('when not same value', function() {
+            it('should return false', function() {
+                $this->matcher->expectValue = 'foo';
+                Assertion::false($this->matcher->match([
+                    'bar' => 'foo'
+                ]));
+            });
+        });
+    });
+
+});
---
+++ src/matcher/ArrayKeyMatcher.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation\matcher;
+
+use expectation\AbstractMatcher;
+use expectation\matcher\annotation\Lookup;
+
+
+class ArrayKeyMatcher extends AbstractMatcher
+{
+
+    /**
+     * @Lookup(name="toHaveKey")
+     * @param mixed $actual
+     */
+    public function match($actual)
+    {
+        $this->actualValue = $actual;
+        return array_key_exists($this->expectValue, $this->actualValue);
+    }
+
+    /**
+     * @return string
+     */
+    public function getFailureMessage()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getNegatedFailureMessage()
+    {
+    }
+
+}
