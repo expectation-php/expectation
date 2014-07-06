71ed76ac47eb6bb7cdc9c09f1a8c6f9d5ce6278d
holyshared <holy.shared.design@gmail.com>
2014-07-06 16:29:35 +0900

    add InclusionMatcher

---
+++ spec/matcher/InclusionMatcherSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Preview\DSL\BDD;
+
+use expectation\matcher\InclusionMatcher;
+use expectation\matcher\Formatter;
+use Assert\Assertion;
+
+describe('InclusionMatcher', function() {
+
+    before_each(function() {
+        $this->matcher = new InclusionMatcher(new Formatter());
+    });
+
+    describe('match', function() {
+        context('when actual is string', function() {
+            context('when expect value is string', function() {
+                it('should return true', function() {
+                    $this->matcher->expectValue = 'foo';
+                    Assertion::true($this->matcher->match('foobar'));
+                });
+            });
+            context('when expect value is array', function() {
+                it('should return true', function() {
+                    $this->matcher->expectValue = ['foo'];
+                    Assertion::true($this->matcher->match('foo'));
+                });
+            });
+        });
+        context('when actual is array', function() {
+            context('when expect value is string', function() {
+                it('should return true', function() {
+                    $this->matcher->expectValue = 'foo';
+                    Assertion::true($this->matcher->match(['foo']));
+                });
+            });
+            context('when expect value is array', function() {
+                it('should return true', function() {
+                    $this->matcher->expectValue = ['foo'];
+                    Assertion::true($this->matcher->match(['foo']));
+                });
+            });
+        });
+    });
+
+});
---
+++ src/matcher/InclusionMatcher.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation\matcher;
+
+use expectation\AbstractMatcher;
+use expectation\matcher\annotation\Lookup;
+
+/**
+ * @package expectation\matcher
+ * @author Noritaka Horio <holy.shared.design@gmail.com>
+ */
+class InclusionMatcher extends AbstractMatcher
+{
+
+    /**
+     * @Lookup(name="toContain")
+     * @param mixed $actual
+     * @return boolean
+     */
+    public function match($actual)
+    {
+        $result = false;
+        $this->actualValue = $actual;
+
+        if (is_string($this->actualValue)) {
+            $included = false;
+            $expectValues = (is_array($this->expectValue))
+                ? $this->expectValue : [$this->expectValue];
+
+            foreach($expectValues as $expectValue) {
+                $result = strpos($this->actualValue, $expectValue);
+                if ($result === false) {
+                    continue;
+                }
+                $included = true;
+                break;
+            }
+            $result = $included;
+
+        } else if (is_array($this->actualValue)) {
+            $included = false;
+            $expectValues = (is_array($this->expectValue))
+                ? $this->expectValue : [$this->expectValue];
+
+            foreach($expectValues as $expectValue) {
+                $result = in_array($expectValue, $this->actualValue);
+                if ($result === false) {
+                    continue;
+                }
+                $included = true;
+                break;
+            }
+            $result = $included;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @Lookup(name="toHaveKey")
+     * @param mixed $actual
+     * @return boolean
+     */
+    public function containsKey($actual)
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getFailureMessage()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getNegatedFailureMessage()
+    {
+    }
+
+}
