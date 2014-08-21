29a01e79e45a81bf2a4016f87ec4f8c6b040cfc7
holyshared <holy.shared.design@gmail.com>
2014-08-21 20:41:16 +0900

    add src/matcher/strategy/StringInclusionStrategy.php

---
+++ spec/matcher/strategy/StringInclusionStrategySpec.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Preview\DSL\BDD;
+
+use Assert\Assertion;
+use expectation\matcher\strategy\InclusionResult;
+use expectation\matcher\strategy\StringInclusionStrategy;
+
+describe('StringInclusionStrategy', function() {
+    before(function() {
+        $this->strategy = new StringInclusionStrategy('foo var');
+    });
+    describe('#match', function() {
+        before(function() {
+            $this->result = $this->strategy->match(['foo', 'bar']);
+        });
+        it('return expectation\matcher\strategy\InclusionResult instance', function() {
+            Assertion::isInstanceOf($this->result, 'expectation\matcher\strategy\InclusionResult');
+        });
+        describe('result', function() {
+            it('has match results', function() {
+                Assertion::count($this->result->getMatchResults(), 1);
+            });
+            it('has unmatch results', function() {
+                Assertion::count($this->result->getUnmatchResults(), 1);
+            });
+        });
+    });
+});
