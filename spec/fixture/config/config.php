c5e12139a33360d1485bd4704f4b11d281374348
holyshared <holy.shared.design@gmail.com>
2014-10-29 09:46:01 +0900

    Add config file fixture

---
+++ spec/fixture/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'classes' => [
+        'expectation\spec\fixture\matcher\basic\FixtureMatcher'
+    ],
+    'namespaces' => [
+        'expectation\spec\fixture\matcher\basic' => __DIR__ . '/../matcher/basic'
+    ]
+];
