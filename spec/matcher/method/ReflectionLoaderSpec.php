9a18cf94458dfd70d812d5aa88eaac0a1c29965c
holyshared <holy.shared.design@gmail.com>
2014-11-03 15:21:37 +0900

    add loader

---
+++ spec/matcher/method/ReflectionLoaderSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use Assert\Assertion;
+use \ReflectionClass;
+use expectation\matcher\method\ReflectionLoader;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+describe('ReflectionLoader', function() {
+
+    beforeEach(function () {
+        $this->className = '\expectation\spec\fixture\matcher\single\FixtureSingleMatcher';
+    });
+    describe('loadFromClass', function() {
+        beforeEach(function () {
+            $loader = new ReflectionLoader(new AnnotationReader());
+            $this->result = $loader->loadFromClass(new ReflectionClass($this->className));
+        });
+        it('return \ArrayIterator instance', function() {
+            Assertion::isInstanceOf($this->result, '\ArrayIterator');
+        });
+    });
+
+});
---
+++ src/matcher/method/ReflectionLoader.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * This file is part of expectation package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace expectation\matcher\method;
+
+use expectation\matcher\annotation\Lookup;
+use Doctrine\Common\Annotations\Reader;
+use PhpCollection\SequenceInterface;
+use PhpCollection\Sequence;
+use PhpCollection\Map;
+use \ReflectionMethod;
+use \ReflectionClass;
+use \AppendIterator;
+
+
+/**
+ * Class ReflectionLoader
+ * @package expectation\matcher\method
+ */
+class ReflectionLoader
+{
+
+    /**
+     * @var \Doctrine\Common\Annotations\Reader
+     */
+    private $annotationReader;
+
+
+    /**
+     * @param \Doctrine\Common\Annotations\Reader $annotationReader
+     */
+    public function __construct(Reader $annotationReader)
+    {
+        $this->annotationReader = $annotationReader;
+    }
+
+    /**
+     * @param ReflectionClass $classReflection
+     * @return \ArrayIterator
+     */
+    public function loadFromClass(ReflectionClass $classReflection)
+    {
+        return $this->parseAnnotations($classReflection);
+    }
+
+    /**
+     * @param \PhpCollection\SequenceInterface $namespaceReflections
+     * @return \AppendIterator
+     */
+    public function loadFromNamespaces(SequenceInterface $namespaceReflections)
+    {
+        $resultMethodReflections = new AppendIterator();
+
+        foreach($namespaceReflections as $namespaceReflection) {
+            $classReflections = $namespaceReflection->getClassReflections();
+
+            $methodReflections = $this->loadFromClasses(new Sequence($classReflections));
+            $resultMethodReflections->append($methodReflections);
+        }
+
+        return $resultMethodReflections;
+    }
+
+
+    /**
+     * @param \PhpCollection\SequenceInterface $classReflections
+     * @return \AppendIterator
+     */
+    public function loadFromClasses(SequenceInterface $classReflections)
+    {
+        $resultMethodReflections = new AppendIterator();
+
+        foreach ($classReflections as $classReflection) {
+            $methodReflections = $this->loadFromClass($classReflection);
+            $resultMethodReflections->append($methodReflections);
+        }
+
+        return $resultMethodReflections;
+    }
+
+    /**
+     * @param ReflectionClass $classReflection
+     * @return \ArrayIterator
+     */
+    private function parseAnnotations(ReflectionClass $classReflection)
+    {
+        $registry = new Map();
+        $methods = $classReflection->getMethods();
+
+        foreach($methods as $method) {
+            $result = $this->parseMethodAnnotations($method);
+            $registry->addMap($result);
+        }
+
+        return $registry->getIterator();
+    }
+
+    /**
+     * @param ReflectionMethod $method
+     * @return Map
+     */
+    private function parseMethodAnnotations(ReflectionMethod $method)
+    {
+        $registry = new Map();
+        $annotations = $this->annotationReader->getMethodAnnotations($method);
+
+        foreach ($annotations as $annotation) {
+            if (!($annotation instanceof Lookup)) {
+                continue;
+            }
+            $registerName = $annotation->getLookupName();
+            $registry->set($registerName, $method);
+        }
+
+        return $registry;
+    }
+
+}
