0fccf54361c4d0ecbbb95863fb6f039e41a41cea
holyshared <holy.shared.design@gmail.com>
2014-11-12 13:20:08 +0900

    Update travis config

--- RoboFile.php
+++ RoboFile.php
@@ -10,12 +10,17 @@ class RoboFile extends Tasks
 
     public function specAll()
     {
-				return $this->taskExec('vendor/bin/peridot --grep *Spec.php spec')->run();
+        return $this->taskExec('vendor/bin/peridot --grep *Spec.php spec')->run();
+    }
+
+    public function phpMetrics()
+    {
+        return $this->taskExec('vendor/bin/phpmetrics src')->run();
     }
 
     public function specCoveralls()
     {
-				$configuration = Configuration::loadFromFile('.coveralls.yml');
+        $configuration = Configuration::loadFromFile('.coveralls.yml');
         $builder = ReportBuilder::fromConfiguration($configuration);
         $builder->build()->save()->upload();
     }
